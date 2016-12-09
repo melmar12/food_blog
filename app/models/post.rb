c5c4585be550fe58be070b5b60b6a80779a9dbe8
Melissa Martinez <melmar12@live.com>
2016-12-09 00:08:03 -0600

    started bootstrap ish theme

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "350x197>", thumb: "100x100>", large: "1500x2100>" }, default_url: "/images/:style/missing.png"
   
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
