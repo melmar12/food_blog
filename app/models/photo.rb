1b8ddbc51f2860bacc81a568976a128d2f39e16a
Melissa Martinez <melmar12@live.com>
2016-12-09 01:56:32 -0600

    using ckeditor instead

--- app/models/photo.rb
+++
@@ -1,7 +0,0 @@
-class Photo < ActiveRecord::Base
-  belongs_to :post
-
-  has_attached_file :image, styles: { medium: "500x700>"}, default_url: "/images/:style/missing.png"
-  
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :photos
+  mount Ckeditor::Engine => '/ckeditor'
   resources :posts
   get 'home/index'
 
