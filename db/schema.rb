0e727687073027436a98ddb49b157689db6f05b4
Melissa Martinez <melmar12@live.com>
2016-12-09 00:38:43 -0600

    multiple photos per blog post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209022544) do
+ActiveRecord::Schema.define(version: 20161209062136) do
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "post_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "photos", ["post_id"], name: "index_photos_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
